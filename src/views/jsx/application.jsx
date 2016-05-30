1bb8fc8fe36774b6b5525ecefe02666aa85c7f6a
ren-motomura <ren.motomura@gmail.com>
2016-05-30 09:05:59 +0900

    Side menu

--- src/views/jsx/application.jsx
+++ src/views/jsx/application.jsx
@@ -2,14 +2,14 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Hello = require('./components/hello.jsx')
 const Button = require('./components/button.jsx')
+const SideMenu = require('./components/side_menu.jsx')
 
 class Application extends React.Component {
   render() {
     return(
-      <div id='application-container'>
-        <div id='header'></div>
-        <div id='side-menu'></div>
-        <div id='main-content'></div>
+      <div className='application-container'>
+        <SideMenu />
+        <div className='main-content'></div>
       </div>
     )
   }
---
+++ src/views/jsx/components/side_menu.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+
+class SideMenu extends React.Component {
+  render() {
+    return(
+      <div className='side-menu'>
+        <div className='side-menu-title'>
+          <h1>IceRink</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+module.exports = SideMenu
